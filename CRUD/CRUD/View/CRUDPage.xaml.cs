afbe38f6199d23fbd1c85200b46f1dbcf38a85a1
RodriguesFundo <rodriguesdomingosf@gmail.com>
2023-01-31 08:12:01 -0800

    Removed from toolbar { tbItemRemove- Because the ItemSelected_Events open a pop ups[options(Edit, Delete)] ListView_ItemSeleted{    Edit(D)    Ddelete(D) } All CRUD operations are done

--- CRUD/CRUD/View/CRUDPage.xaml.cs
+++ CRUD/CRUD/View/CRUDPage.xaml.cs
@@ -3,6 +3,7 @@ using CRUD.view;
 using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -28,22 +29,21 @@ namespace CRUD.View
             listView.ItemsSource = conn.Table<User>();
         }
 
+        //Evento disparado quando o toolbarItemSearch e clicado (Mostrar a barra de pesquisa)
         private void tbItemSearch_Clicked(object sender, EventArgs e)
         {
             searchBar.IsVisible = true;
+            searchBar.Focus();
         }
 
-        private void tbItemRemove_Clicked(object sender, EventArgs e)
-        {
-            DisplayAlert("Removed", "", "OK");
-        }
-
+        //Evento disparado quando o toolbarItemAdd e clicado (Abrir um formulario para cadastro de novo usuario)
         private void tbItemAdd_Clicked(object sender, EventArgs e)
         {
             Navigation.PushModalAsync(new CreatePage());
 
         }
 
+        //Evento disparado quando a barra de pesquisa e usada, ela retorna os dados (da BD) que estao a ser introduzidos
         private void searchBar_TextChanged(object sender, TextChangedEventArgs e)
         {
             _searchText = e.NewTextValue.ToLower();
@@ -53,16 +53,33 @@ namespace CRUD.View
             conn.Close();
         }
 
+        //Evento disparado quando o toolbarItemExit e clicado (Sair do sistema)
         private void tbItemExit_Clicked(object sender, EventArgs e)
         {
             Navigation.PopToRootAsync();
         }
 
-        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        //Evento disparado quando um item da listView e selecionado
+        private async void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            itemSelected= (User)e.SelectedItem;
+            itemSelected= e.SelectedItem as User;
+            if (itemSelected != null) {
+                string action = await DisplayActionSheet("", "Cancel", null, "Edit", "Delete");
+                if (action.Equals("Edit"))
+                {
+                    await Navigation.PushModalAsync(new UpdateUserPage(itemSelected));
+                }
+                else if (action.Equals("Delete"))
+                {
+                    var conn = new SQLiteConnection(DBpath);
+                    await DisplayAlert("Deleted", itemSelected.Name, "Ok");
+                    conn.Table<User>().Delete(x => x.Username.Equals(itemSelected.Username));
+                    conn.Close();
 
-            Navigation.PushModalAsync(new UpdateUserPage(itemSelected));
+                }
+            }
+
         }
+
     }
 }
